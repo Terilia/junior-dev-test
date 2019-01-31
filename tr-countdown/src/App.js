6bee75b1b5d42d0f3d1c95baeece330613446355
Terilia <Terilia@users.noreply.github.com>
2019-01-31 18:50:26 +0000

    added Material UI + countdown-now

--- tr-countdown/src/App.js
+++ tr-countdown/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Countdown from 'react-countdown-now';
+import Button from '@material-ui/core/Button';
+
 
 class App extends Component {
   render() {
@@ -9,16 +12,19 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <Countdown date={new Date("March 15 2019 17:00:00 GMT +1").getTime()} />
+          <Button variant="contained" color="primary">
+          Hello World
+          </Button>
           </p>
-          <a
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
