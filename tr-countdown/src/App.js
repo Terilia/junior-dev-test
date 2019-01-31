f250e638ee46e6f91e33a416ed2fcb9196436e6b
Terilia <Terilia@users.noreply.github.com>
2019-01-31 22:01:47 +0000

    added placeholder buttons

--- tr-countdown/src/App.js
+++ tr-countdown/src/App.js
@@ -2,29 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import Countdown from 'react-countdown-now';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import buttonTeaser from './teaserbutton.png';
+import { Button } from '@material-ui/core';
+
 
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
+        <CssBaseline />
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          <Countdown date={new Date("March 15 2019 17:00:00 GMT +1").getTime()} />
-          <Button variant="contained" color="primary">
-          Hello World
-          </Button>
-          </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
+          <p><Countdown date={new Date("March 15 2019 17:00:00 GMT +1").getTime()}/></p>
+          <p>Reveal Event April 27TH</p>
+          <Button href="https://youtube.com">Watch Teaser</Button>
+          <Button>Add to Calendar</Button>
+          <Button>Newsletter Sign Up</Button>
+          <Button>Facebook</Button>
+          <Button>Twitter</Button>
+          <Button>Google Plus</Button>
+          <Button>Youtube</Button>
+          <Button>Instagram</Button>
         </header>
       </div>
     );
