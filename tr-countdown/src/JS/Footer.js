962aec334cdba9d7faa0d61fced2af7d9089d051
Terilia <Terilia@users.noreply.github.com>
2019-02-03 18:20:50 +0000

    footer images centered, footer links centered, minimal header adjustments.

--- tr-countdown/src/JS/Footer.js
+++ tr-countdown/src/JS/Footer.js
@@ -17,36 +17,36 @@ class Footer extends Component {
 	render() {
 		return (
 		<Container>
-			<Row className="legal-container">
-				<Col className="legal-image"><Image src={ESRBLogo}/></Col>
-				<Col className="legal-image"><Image src={XboxLogo}/></Col>
-				<Col className="legal-image"><Image src={Ps4Logo}/></Col>
-				<Col className="legal-image"><Image src={SteamLogo}/></Col>
-				<Col className="legal-image"><Image src={PCLogo}/></Col>
+			<Row className="justify-content-md-center link-list">
+				<Col className="text-center legal-image"><Image src={ESRBLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={XboxLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={Ps4Logo}/></Col>
+				<Col className="text-center legal-image"><Image src={SteamLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={PCLogo}/></Col>
 			</Row>
 			<Row>
-				<Col className="legal-image"><Image src={CrystalLogo}/></Col>
-				<Col className="legal-image"><Image src={SquareEnixLogo}/></Col>
-				<Col className="legal-image"><Image src={EidosLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={CrystalLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={SquareEnixLogo}/></Col>
+				<Col className="text-center legal-image"><Image src={EidosLogo}/></Col>
 			</Row>
 			<Row>
 				<Col><div className="legal-text">SHADOW OF THE TOMB RAIDER © 2019 Square Enix Limited.	Developed by Eidos Interactive Corporation. All rights reserved.	SHADOW OF THE TOMB RAIDER, TOMB RAIDER, CRYSTAL DYNAMICS, the CRYSTAL DYNAMICS logo, EIDOS-MONTRÉAL, the EIDOS-MONTRÉAL logo, and LARA CROFT are registered trademarks or trademarks of Square Enix Limited. SQUARE ENIX and the SQUARE ENIX logo are registered trademarks or trademarks of Square Enix Holdings Co., Ltd. XBOX, XBOX ONE, the Games for Windows logo and Xbox logos are registered trademarks or trademarks of the Microsoft group of companies and are used under license. “PS4” and “PlayStation” are registered trademarks of Sony Interactive Entertainment Inc. "Steam” is a trademark of the same company. ©2019 Valve Corporation. Steamworks and the Steamworks logo are trademarks and/or registered trademarks of Valve Corporation. All other trademarks are the property of their respective owners.</div></Col>
 			</Row>
 			<Row className="justify-content-md-center link-list">
 				<Col xs={6} md="auto" className="text-center bottom-nav"><a href="http://square-enix-games.com">Cookie Policy</a></Col>
-				<Col md="auto" className="bottom-nav colon">●</Col>
+				<Col md="auto" className="text-center bottom-nav colon">●</Col>
 				<Col xs={6} md="auto" className="text-center bottom-nav"><a href="http://square-enix-games.com">Terms of Use</a></Col>
-				<Col md="auto" className="bottom-nav colon">●</Col>
+				<Col md="auto" className="text-center bottom-nav colon">●</Col>
 				<Col xs={6} md="auto" className="text-center bottom-nav"><a href="http://square-enix-games.com">Privacy Policy</a></Col>
-				<Col md="auto" className="bottom-nav colon">●</Col>
+				<Col md="auto" className="text-center bottom-nav colon">●</Col>
 				<Col xs={6} md="auto" className="text-center bottom-nav"><a href="http://square-enix-games.com">Support</a></Col>
-				<Col md="auto" className="bottom-nav colon">●</Col>
+				<Col md="auto" className="text-center bottom-nav colon">●</Col>
 				<Col xs={6} md="auto" className="text-center bottom-nav"><a href="http://square-enix-games.com">Material Usage</a></Col>
 			</Row>
 		</Container>
 		);
 	}
-	}
+}
 	
 	export default Footer;
 	
